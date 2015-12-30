4f53dbafde82d724c1d2d2d4c62c27dfd0ee1acc
StDante <380931224952@yandex.ru>
2015-12-30 09:22:57 +0200

    no message

--- IDC GitHub/SRC/IDCLection3Structure/IDCLection3Structure.c
+++ IDC GitHub/SRC/IDCLection3Structure/IDCLection3Structure.c
@@ -8,14 +8,3 @@
 
 #include <stdio.h>
 #include "IDCLection3Structure.h"
-
-void IDCPrintStructureIDCPatientSize() {
-    printf("Size of structure 'IDCPatient' is %lu\n", sizeof(IDCPatient));
-    
-}
-
-void IDCPrintStructureIDCOptimisationPatientStructureSize() {
-    printf("Size of structure 'IDCOptimisationPatientStructure' is %lu\n",
-                                    sizeof(IDCOptimisationPatientStructure));
-    
-}
--- IDC GitHub/Test/IDCLection3StructureTest/IDCLection3StructureTest.c
+++ IDC GitHub/Test/IDCLection3StructureTest/IDCLection3StructureTest.c
@@ -6,4 +6,16 @@
 //  Copyright © 2015 Alexandr Altukhov. All rights reserved.
 //
 
+#include <stdio.h>
 #include "IDCLection3StructureTest.h"
+
+void IDCPrintStructureIDCPatientSizeTest() {
+    printf("Size of structure 'IDCPatient' is %lu\n", sizeof(IDCPatient));
+    
+}
+
+void IDCPrintStructureIDCOptimisationPatientStructureSizeTest() {
+    printf("Size of structure 'IDCOptimisationPatientStructure' is %lu\n",
+           sizeof(IDCOptimisationPatientStructure));
+    
+}
