e8ef39d37086b158e5ea5082b8673a2162a36250
StDante <380931224952@yandex.ru>
2016-02-19 09:41:49 +0200

    ObjC. new class IDCCreature, added some methods.

--- ObjC/SRC/IDCTask1Creature/NSObject+IDCTask1Creature.h
+++ ObjC/SRC/IDCTask1Creature/NSObject+IDCTask1Creature.h
@@ -8,6 +8,30 @@
 
 #import <Foundation/Foundation.h>
 
-@interface NSObject (IDCTask1Creature)
+@interface IDCCreature : NSObject;
+
+typedef enum {
+    kIDCUnknownGender,
+    kIDCMale,
+    kIDCFemale
+} IDCGender;
+
+@property (atomic, retain, readonly)    NSArray<IDCCreature *> *children;
+@property (nonatomic, copy)             NSString  *name;
+@property (nonatomic, assign, readonly) IDCGender gender;
+@property (nonatomic, assign)           NSNumber  *age;
+@property (nonatomic, assign)           NSNumber  *weight;
+
+- (instancetype)initWithName:(NSString *)name gender:(IDCGender)gender;
+
+- (void)startFighting;
+- (void)stopFighting;
+
+- (instancetype)bornChildwithName:(NSString *)name gender:(IDCGender)gender;
+
+- (void)addChild:(IDCCreature *)child;
+- (void)removeChild:(IDCCreature *)child;
+
+- (void)sayHi;
 
 @end
--- ObjC/SRC/IDCTask1Creature/NSObject+IDCTask1Creature.m
+++ ObjC/SRC/IDCTask1Creature/NSObject+IDCTask1Creature.m
@@ -8,6 +8,74 @@
 
 #import "NSObject+IDCTask1Creature.h"
 
-@implementation NSObject (IDCTask1Creature)
+@interface IDCCreature ()
+
+@property (nonatomic, retain) NSMutableArray<IDCCreature *> *mutableChildren;
+
+@property (nonatomic, assign, readwrite) IDCGender gender;
+
+@end
+
+@implementation IDCCreature
+
+#pragma mark -
+#pragma mark Initialization and Deallocation
+
+- (void)dealloc {
+    self.name            = nil;
+    self.mutableChildren = nil;
+    
+    [super dealloc];
+}
+
+- (instancetype)initWithName:(NSString *)name gender:(IDCGender)gender {
+    self        = [[[IDCCreature  alloc] init] autorelease];
+    
+    self.name   = name;
+    self.gender = gender;
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark Accessor
+
+#pragma mark -
+#pragma mark Public
+
+- (instancetype)bornChildwithName:(NSString *)name gender:(IDCGender)gender {
+    if (self.gender == kIDCFemale) {
+        IDCCreature *child = [self initWithName:name gender:gender];
+        return child;
+    }
+    
+    NSLog(@"Real man doesn't give a birth, real man take lifes");
+    [self startFighting];
+        
+    return nil;
+}
+
+- (void)startFighting {
+// Idea for method realesation: Man going to war, he can die randomly.
+// If he die's  send - release, else - stop fight
+}
+- (void)stopFighting {
+// When Man comeback from war send method sayHi
+}
+
+- (void)addChild:(IDCCreature *)child {
+    
+}
+- (void)removeChild:(IDCCreature *)child {
+    
+}
+
+- (void)sayHi {
+    
+}
+
+
+#pragma mark -
+#pragma mark Private
 
 @end
