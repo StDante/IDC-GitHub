338c44b0488d06bcbd61b644022f0fb26346ad86
StDante <380931224952@yandex.ru>
2016-03-07 13:36:01 +0200

    added realization for category IDCAplhabet

--- ObjC/SRC/IDCCategory/IDCTask5Alphabet/NSObject+IDCAlphabet.h
+++ ObjC/SRC/IDCCategory/IDCTask5Alphabet/NSObject+IDCAlphabet.h
@@ -9,10 +9,20 @@
 #import <Foundation/Foundation.h>
 
 @interface NSObject (IDCAlphabet)
+@property (nonatomic, readonly) NSString * alphabetString;
 
 + (instancetype)alphabetWithRange:(NSRange)range;
 + (instancetype)alphabetWithCharRange:(unichar)firstValue lastValue:(unichar)lastValue;
 + (instancetype)alphabetWithString:(NSString *)string;
 + (instancetype)alphabetWithAlphabets:(NSArray *)alphabets;
+
+- (instancetype)initWithRange:(NSRange)range;
+- (instancetype)initWithCharRange:(unichar)firstValue lastValue:(unichar)lastValue;
+- (instancetype)initWithString:(NSString *)string;
+- (instancetype)initWithAlphabets:(NSArray *)alphabets;
+
++ (instancetype)lowercaseAlphabet;
++ (instancetype)uppercaseAlphabet;
++ (instancetype)numericAlphabet;
 
 @end
--- ObjC/SRC/IDCCategory/IDCTask5Alphabet/NSObject+IDCAlphabet.m
+++ ObjC/SRC/IDCCategory/IDCTask5Alphabet/NSObject+IDCAlphabet.m
@@ -8,6 +8,72 @@
 
 #import "NSObject+IDCAlphabet.h"
 
+
 @implementation NSObject (IDCAlphabet)
+
+@dynamic alphabetString;
+
+#pragma mark -
+#pragma mark Class Methods
+
++ (instancetype)alphabetWithRange:(NSRange)range {
+    return [[[self alloc] initWithRange:range] autorelease];
+}
+
++ (instancetype)alphabetWithCharRange:(unichar)firstValue lastValue:(unichar)lastValue {
+    return [[[self alloc] initWithCharRange:firstValue lastValue:lastValue] autorelease];
+}
+
++ (instancetype)alphabetWithString:(NSString *)string {
+    return [[[self alloc] initWithString:string] autorelease];
+}
+
++ (instancetype)alphabetWithAlphabets:(NSArray *)alphabets {
+    return [[[self alloc] initWithAlphabets:alphabets] autorelease];
+}
+
+#pragma mark -
+#pragma mark Initialization and Deallocation
+
+- (instancetype)initWithRange:(NSRange)range {
+    [self autorelease];
+    
+    return [[[IDCRangeAlphabet alloc] initWithRange:range] autorelease];
+}
+
+- (instancetype)initWithCharRange:(unichar)firstValue lastValue:(unichar)lastValue {
+    [self autorelease];
+    
+    return [[[IDCRangeAlphabet alloc] initWithRange:NSMakeRange(firstValue, lastValue - firstValue + 1)]
+                                                                                            autorelease];
+}
+
+- (instancetype)initWithString:(NSString *)string {
+    [self autorelease];
+    
+    return [[[IDCStringAlphabet alloc] initWithString:string] autorelease];
+}
+
+- (instancetype)initWithAlphabets:(NSArray *)alphabets {
+    [self autorelease];
+    
+    return [[[IDCClusterAlphabet alloc] initWithAlphabets:alphabets] autorelease];
+    
+}
+
+#pragma mark -
+#pragma mark Alphabets
+
++ (instancetype)lowercaseAlphabet {
+    return [self alphabetWithCharRange:'a' lastValue:'z'];
+}
+
++ (instancetype)uppercaseAlphabet {
+    return [self alphabetWithCharRange:'A' lastValue:'Z'];
+}
+
++ (instancetype)numericAlphabet {
+    return [self alphabetWithCharRange:'0' lastValue:'9'];
+}
 
 @end
